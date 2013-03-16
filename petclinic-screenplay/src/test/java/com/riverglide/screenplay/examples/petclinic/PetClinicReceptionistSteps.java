acdabce8699454d03efc37d77be4428f389131cd
antonymarcano <antony@antonymarcano.com>
2013-03-16 19:31:53 +0000

    Cleaning up after ourselves (after scenarios for now); with @jan-molak

--- petclinic-screenplay/src/test/java/com/riverglide/screenplay/examples/petclinic/PetClinicReceptionistSteps.java
+++ petclinic-screenplay/src/test/java/com/riverglide/screenplay/examples/petclinic/PetClinicReceptionistSteps.java
@@ -1,5 +1,6 @@
 package com.riverglide.screenplay.examples.petclinic;
 
+import cucumber.api.java.After;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.openqa.selenium.By;
@@ -16,12 +17,18 @@ public class PetClinicReceptionistSteps {
     @When("^I get started$")
     public void I_get_started() throws Throwable {
         driver = new FirefoxDriver();
-        driver.get("http://localhost:9966/petclinic");
+        String petClinicHomePage = "http://localhost:9966/petclinic";
+        driver.get(petClinicHomePage);
     }
 
     @Then("^I should feel welcome$")
     public void I_should_feel_welcome() throws Throwable {
         String message = driver.findElement(By.cssSelector("h2")).getText();
         assertThat(message, is("Welcome"));
+    }
+
+    @After
+    public void closeBrowser() {
+        driver.quit();
     }
 }
