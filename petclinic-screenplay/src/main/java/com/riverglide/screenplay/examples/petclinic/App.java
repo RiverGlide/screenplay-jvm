106a665e84fa9f150721bad844fc12f08496124e
jan-molak <jan.molak@gmail.com>
2013-03-16 17:02:27 +0000

    We have a place to start; with @antonymarcano

---
+++ petclinic-screenplay/src/main/java/com/riverglide/screenplay/examples/petclinic/App.java
@@ -0,0 +1,13 @@
+package com.riverglide.screenplay.examples.petclinic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ screenplay/src/main/java/com/riverglide/screenplay/App.java
@@ -0,0 +1,13 @@
+package com.riverglide.screenplay;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
