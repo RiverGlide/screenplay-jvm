a79ea0b6a1ce5375198c467eb35eca3abc45ff52
antonymarcano <antony@antonymarcano.com>
2013-03-20 07:16:05 +0000

    Java has enough repetition as it is. No need to make it worse

--- petclinic-screenplay/src/main/java/com/riverglide/screenplay/examples/petclinic/activity/Start.java
+++ petclinic-screenplay/src/main/java/com/riverglide/screenplay/examples/petclinic/activity/Start.java
@@ -3,8 +3,10 @@ package com.riverglide.screenplay.examples.petclinic.activity;
 import com.riverglide.screenplay.Perform;
 import com.riverglide.screenplay.examples.petclinic.activity.task.StartHelpingACustomer;
 
+import static com.riverglide.screenplay.examples.petclinic.activity.task.StartHelpingACustomer.startHelpingACustomer;
+
 public class Start {
     public static Perform helpingACustomer() {
-        return StartHelpingACustomer.startHelpingACustomer();
+        return startHelpingACustomer();
     }
 }
