d9b6bf1750f0bbc778a27985a3faaa5bdd2611c8
antonymarcano <antony@antonymarcano.com>
2013-03-21 17:03:33 +0000

    Highlighting that many types of things can be remembered

--- screenplay/src/test/java/com/riverglide/screenplay/ActorTest.java
+++ screenplay/src/test/java/com/riverglide/screenplay/ActorTest.java
@@ -2,10 +2,30 @@ package com.riverglide.screenplay;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class ActorTest {
+
+    @Test
+    public void remembersAnyOldThing() throws Exception {
+        List<ItemToRemember<?>> items = new ArrayList<ItemToRemember<?>>();
+        items.add(new ItemToRemember<Float>(new Float(1.234)));
+        items.add(new ItemToRemember<Boolean>(false));
+
+        for(ItemToRemember<?> remembered : items) {
+            Actor inARole = new Actor();
+            inARole.remember(remembered);
+
+            assertThat("for example [" + remembered.item().toString() + "]",
+                    inARole.getIt(), is(remembered.item())
+            );
+        }
+    }
 
     @Test
     public void rememberAString() throws Exception {
@@ -20,10 +40,10 @@ public class ActorTest {
     @Test
     public void rememberAnInteger() throws Exception {
         Actor inARole = new Actor();
-        ItemToRemember<Integer> item = new ItemToRemember<Integer>(1);
+        ItemToRemember<Integer> remembered = new ItemToRemember<Integer>(1);
 
-        inARole.remember(item);
+        inARole.remember(remembered);
 
-        assertThat(inARole.getIt(), is(item.item()));
+        assertThat(inARole.getIt(), is(remembered.item()));
     }
 }
